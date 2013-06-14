4d3f4699ea3d1f4256f756ab34ca59474689fd27
mofumofu3n <mofumofu3n@gmail.com>
2013-06-15 01:59:41 +0900

    コード整形

--- src/com/Ichif1205/anrakutei/Utils.java
+++ src/com/Ichif1205/anrakutei/Utils.java
@@ -3,10 +3,14 @@ package com.Ichif1205.anrakutei;
 import android.content.Context;
 import android.graphics.Typeface;
 import android.media.AudioManager;
+import android.util.Log;
 
 public class Utils {
+	private static final String TAG = Utils.class.getSimpleName();
+
 	/**
 	 * フォントを取得
+	 * 
 	 * @param context
 	 * @return
 	 */
@@ -14,14 +18,16 @@ public class Utils {
 		return Typeface.createFromAsset(context.getAssets(),
 				"fonts/BallsoOnTheRampage.ttf");
 	}
-	
+
 	/**
 	 * BGMを流す
+	 * 
 	 * @param context
 	 * @return boolean
 	 */
 	public static boolean isRingerEnable(Context context) {
-		AudioManager am = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+		AudioManager am = (AudioManager) context
+				.getSystemService(Context.AUDIO_SERVICE);
 		final int audioMode = am.getRingerMode();
 		if (audioMode == AudioManager.RINGER_MODE_NORMAL) {
 			return true;
