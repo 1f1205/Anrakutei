11547a3065691eec0ded01415c25afcacda35d30
mofumofu3n <mofumofu3n@gmail.com>
2013-06-15 19:24:43 +0900

    ランキング画面に広告を表示

--- src/com/Ichif1205/anrakutei/RankingActivity.java
+++ src/com/Ichif1205/anrakutei/RankingActivity.java
@@ -119,12 +119,4 @@ public class RankingActivity extends Activity {
 			}
 		}
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_ranking, menu);
-		return true;
-	}
-
 }
